5b76f97bce8c2c7abde523eedcd2630892c6f09f
Miguel Castillo <manchagnu@gmail.com>
2014-11-14 18:17:49 -0500

    Added function names to the error handlers

--- test.js
+++ test.js
@@ -13,13 +13,14 @@ var type        = nconf.get("type")         || nconf.get("AZURE_STORAGE_TYPE");
 // Prepare an azure container with provided name in order to upload some files
 var azureContainer = new AzureContainer(container, accountName, accountKey);
 
+
 ///
 /// Run sequence
 ///
 initializeContainer(type)
-  .then(uploadFile, reportError)
-  .then(listFiles, reportError)
-  .catch(reportError);
+  .then(uploadFile, reportError("initializeContainer"))
+  .then(listFiles, reportError("uploadFile"))
+  .catch(reportError("listFiles"));
 
 
 /**
@@ -57,7 +58,9 @@ function listFiles() {
 }
 
 
-function reportError(error) {
-  console.log(error);
+function reportError(from) {
+  return function errorHandler(error) {
+    console.log(from, error);
+  };
 }
 
