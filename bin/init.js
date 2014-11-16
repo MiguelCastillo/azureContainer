4f689161bdee95935e21354ec1e160586b505123
Miguel Castillo <manchagnu@gmail.com>
2014-11-16 05:23:06 -0500

    Adding flag to init to print `--all` settings.

--- bin/init.js
+++ bin/init.js
@@ -9,48 +9,54 @@ nconf
   .argv(confalias)
   .file({ file: '.zure.json' });
 
-// Get args...
-var options = getOptions();
-var args    = getArgsAsString();
-var print   = nconf.get('print');
+function processSettings() {
+  // Get args...
+  var options  = getOptions();
+  var files    = getArgsAsString();
+  var print    = nconf.get('print');
+  var printAll = nconf.get('all');
 
-options.forEach(function(key) {
-  var value = nconf.get(key);
+  options.forEach(function(key) {
+    var value = nconf.get(key);
 
-  if (value !== true) {
-    nconf.set(key, value);
-  }
-  else {
-    nconf.clear(key);
-  }
-});
-
-
-if (!options.length) {
-  nconf.set('file', '"' + getArgsAsString() + '"');
-  nconf.set('f', '"' + getArgsAsString() + '"');
-}
-
-
-if ((!args && !options.length) || print) {
-  printSettings();
-}
-else {
-  nconf.save(function (err) {
-    if (err) {
-      console.dir(err);
+    if (value !== true) {
+      nconf.set(key, value);
     }
     else {
-      printSettings();
+      nconf.clear(key);
     }
   });
+
+  if (!options.length) {
+    nconf.set('file', files);
+    nconf.set('f', files);
+  }
+
+  if ((!files && !options.length) || print || printAll) {
+    printSettings(printAll);
+  }
+  else {
+    nconf.save(function (err) {
+      if (err) {
+        console.dir(err);
+      }
+      else {
+        printSettings(printAll);
+      }
+    });
+  }
 }
 
-
-function printSettings() {
-  fs.readFile('.zure.json', function (err, data) {
-    console.dir(JSON.parse(data.toString()));
-  });
+function printSettings(printAll) {
+  if (printAll) {
+    nconf.env();
+    console.dir(nconf.get());
+  }
+  else {
+    fs.readFile('.zure.json', function (err, data) {
+      console.dir(JSON.parse(data.toString()));
+    });
+  }
 }
 
 
@@ -65,3 +71,6 @@ function getOptions() {
 function getArgsAsString() {
   return nconf.stores.argv.get('_').join(",");
 }
+
+
+processSettings();
